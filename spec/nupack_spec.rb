736db9332258e1e5e1c22a2267c24830a7f6615f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-16 18:26:03 -0400

    set up nupack_spec requirements

--- spec/nupack_spec.rb
+++ spec/nupack_spec.rb
@@ -1,31 +1,6 @@
-class Job
-	@@flat = 1.05
-	@@markup = {
-		"drugs" => 7.5,
-		"electronics" => 2,
-		"food" => 13
-	}
+require "rspec"
+require_relative "../nulogy"
 
-	attr_accessor :base, :numberPeople, :type
-
-	def initialize(base,numberPeople,type)
-		@base = base
-		@numberPeople = numberPeople
-		@type = type
-	end
-
-	def totalRounded
-		(base * markUp * 100).round / 100.0
-	end
-
-	private
-	def base
-		@base * @@flat
-	end
-
-	def markUp
-		numPercent = @numberPeople * 1.2
-		((@@markup[@type] ? @@markup[@type] : 0 ) + numPercent) / 100 + 1
-	end
+RSpec.describe Job, "#totalRounded" do
 
 end
