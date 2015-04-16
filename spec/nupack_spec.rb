f0a0cb69bb9f86dfc6ccd10ea6c7dd54ec112154
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-04-16 18:06:59 -0400

    Created class variables, private methods to handle different parts of total job calculation

--- spec/nupack_spec.rb
+++ spec/nupack_spec.rb
@@ -1,6 +1,24 @@
 class Job
+	@@flat = 1.05
+	@@markup = {
+		"drugs" => 7.5,
+		"electronics" => 2,
+		"food" => 13
+	}
+
+	attr_accessor :base, :numberPeople, :type
+
 	def initialize(base,numberPeople,type)
+	end
 
 	def total
 	end
+
+	private
+	def base
+	end
+
+	def markUp
+	end
+
 end
